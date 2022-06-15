28852d9eea5328639ef15f85401c083014795ab7
noelaaa <2274976814@qq.com>
2022-06-15 17:04:03 +0800

    add html

--- mail.php
+++ mail.php
@@ -1,5 +1,5 @@
 ﻿<?php
-require_once("mail.inc.php");
+require_once("/mail.inc.php");
 $smtp = new smtp("smtp.qq.com",465,true,"2274976814@qq.com","emuifckbgbotebhf","2274976814@qq.com");//发件人信箱信息
 $smtp->debug = false;//是否显示发送的调试信息 FALSE or TRUE
 $mailto="2274976814@qq.com";//收件人信箱
@@ -7,9 +7,8 @@ $mailsubject="web Message";
 $mailfrom="HR";
 $mailbody="Name=".$_POST["q1"]."<br>";
 $mailbody=$mailbody."Phone No=".$_POST["q2"]."<br>";
-$mailbody=$mailbody."Suburb=".$_POST["q3"]."<br>";
 $mailbody=$mailbody."email=".$_POST["email"]."<br>";
-$mailbody=$mailbody."Message=".$_POST["q4"]."<br>";
+$mailbody=$mailbody."Message=".$_POST["q3"]."<br>";
 //其他的表单项目以此类推
 $mailtype 		= 	"HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
 $mailsubject 	= 	'=?UTF-8?B?'.base64_encode($mailsubject).'?=';//邮件主题

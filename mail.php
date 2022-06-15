342c8ad21fe58dd58e5b8843ca0bec1007de9239
noelaaa <2274976814@qq.com>
2022-06-15 17:06:55 +0800

    add html

--- mail.php
+++ mail.php
@@ -5,10 +5,10 @@ $smtp->debug = false;//是否显示发送的调试信息 FALSE or TRUE
 $mailto="2274976814@qq.com";//收件人信箱
 $mailsubject="web Message";
 $mailfrom="HR";
-$mailbody="Name=".$_POST["q1"]."<br>";
-$mailbody=$mailbody."Phone No=".$_POST["q2"]."<br>";
-$mailbody=$mailbody."email=".$_POST["email"]."<br>";
-$mailbody=$mailbody."Message=".$_POST["q3"]."<br>";
+$mailbody="Name=".$_GET["q1"]."<br>";
+$mailbody=$mailbody."Phone No=".$_GET["q2"]."<br>";
+$mailbody=$mailbody."email=".$_GET["email"]."<br>";
+$mailbody=$mailbody."Message=".$_GET["q3"]."<br>";
 //其他的表单项目以此类推
 $mailtype 		= 	"HTML";//邮件格式（HTML/TXT）,TXT为文本邮件
 $mailsubject 	= 	'=?UTF-8?B?'.base64_encode($mailsubject).'?=';//邮件主题
